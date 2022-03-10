ce2826c282aa6bf5ce5ac2760e0fd207d42f6f3e
Thalita <thalita@gmail.com>
2022-03-10 18:06:55 -0300

    aperfeiçoado

--- fundamentos/bloco-4-javascript-e-logica-de-programacao/dia-4-javascript-ojetos-e-funcoes/exercicio1.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programacao/dia-4-javascript-ojetos-e-funcoes/exercicio1.js
@@ -3,4 +3,4 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-    console.log("Bem vinda" + "," + " " + info.personagem + "!");
+    console.log("Bem-vinda" + "," + " "  + info.personagem);
